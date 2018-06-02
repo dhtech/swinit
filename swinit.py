200b75998529bf0ff435c7db4cf2cf62696bdbed
Christian Svensson <blue@cmd.nu>
2018-06-02 20:19:05 +0000

    wait for boot and configure basic

--- swinit.py
+++ swinit.py
@@ -9,7 +9,7 @@ import sys
 FLAGS = flags.FLAGS
 flags.DEFINE_string('serial', '/dev/ttyUSB0', 'Serial device to manage')
 flags.DEFINE_integer('baud', 9600, 'Serial baud rate')
-flags.DEFINE_integer('timeout', 60, 'Device timeout before resetting state')
+flags.DEFINE_integer('timeout', 180, 'Device timeout before resetting state')
 flags.DEFINE_bool('boot', True, 'Execute boot after init')
 
 
@@ -112,29 +112,57 @@ class Device(object):
     """Boot device."""
     self.port.write(b'boot\n')
 
-  def set_switch_number(self, num):
+  def set_switch_number(self, num, prio):
     """Configure switch stack identity."""
     if self.model.startswith('WS-C3850-'):
       self.port.write(b'set SWITCH_NUMBER ' + str(num).encode() + b'\n')
       self._read_line(['switch: '])
+      self.port.write(b'set SWITCH_PRIORITY ' + str(prio).encode() + b'\n')
+      self._read_line(['switch: '])
+
+  def wait_for_boot_complete(self):
+    """Wait for system bootup complete."""
+    cisco_config_prompt = (
+            'Would you like to enter the initial configuration dialog\? \[yes/no\]: ')
+    cisco_booted = 'Press RETURN to get started'
+    line = self._read_line([cisco_config_prompt, cisco_booted])
+    if line == cisco_config_prompt:
+      self.port.write(b'no\n')
+    return
+
+  def configure(self):
+    """Bring the device to a state where swboot can configure it."""
+    # Password recovery on Cisco devices shuts down all interfaces,
+    # so we have to bring them up explicitly and then reload for
+    # auto install to do its thing
+    if self.model.startswith('WS-C3850-') and self.mgmt_if_status:
+      self.poke()
+      self.port.write(b'en\n')
+      self.port.write(b'conf t\n')
+      self.port.write(b'int vlan 1\n')
+      self.port.write(b'no shut\n')
+      self.port.write(b'end\n')
+      self.port.write(b'reload\n')
+      self.port.write(b'no\n')
+      self.port.write(b'\n')
 
 
 class Events(object):
 
   def __init__(self):
-    self.last_state = 'reset'
+    self.last_state = 'timeout'
 
   def detected(self):
     """Play discovery sound to inform operator we have it from here."""
     self.last_state = 'detected'
     os.system('aplay detected.wav')
 
-  def reset(self):
+  def timeout(self):
     """Play reset sound to inform operator we're ready again."""
     # Never loop this if we haven't detected a device since last time
-    if self.last_state == 'reset':
+    if self.last_state == 'timeout':
       return
-    self.last_state = 'reset'
+    self.last_state = 'timeout'
     os.system('aplay reset.wav')
 
 
@@ -169,22 +197,31 @@ def main(argv):
         # otherwise no 2.
         if device.mgmt_if_status:
           print('Switch is no 1')
-          device.set_switch_number(1)
+          device.set_switch_number(1, 14)
         else:
           print('Switch is no 2')
-          device.set_switch_number(2)
+          device.set_switch_number(2, 1)
 
       device.clear_config()
       print('Done')
-      if FLAGS.boot:
-        device.boot()
-      else:
+      if not FLAGS.boot:
         print('No-boot mode, sleeping for 10 seconds instead')
         import time
         time.sleep(10)
+        continue
+
+      device.boot()
+
+      # Boot and answer any init prompts
+      device.wait_for_boot_complete()
+
+      # Install basic configure for the device for it to continue with
+      # using swboot for real configuration
+      device.configure()
+
     except DeviceTimeoutError:
       print('Device timed out, resetting state')
-      events.reset()
+      events.timeout()
 
 
 if __name__ == '__main__':
